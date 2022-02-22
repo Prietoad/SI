e18a0df7923ffe36e55ae6497ca24f7523b1ff50
Diego <diegoprieto9080@gmail.com>
2022-02-22 22:00:03 +0100

    Practica 1 con extra

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # SI
-Practices from Seguridad Infórmatica ULE
-All files are python scripts that can be executed with:
-python3 <filename>
+Practices from Seguridad Infórmatica ULE .All files are python scripts that can be executed with: 'python3 "filename"'
--- practica1.py
+++ practica1.py
@@ -4,8 +4,15 @@ import math as ma
 import numpy as np
 from numpy import true_divide
 
+#Funcion que cuenta el numero de apariciones de cada simbolo de la fuente y lo guarda en una lista
+def countAppereances(fuente,listNum,listSymbols):
+    for symbol in listSymbols:
+        num = fuente.count(symbol)
+        listNum.append(num)
+
 #Lee el archivo de texto
 f = open("datos_1.txt")
+fuente = f.read()
 
 #Listas
 listAll= []
@@ -14,18 +21,17 @@ listNumber = []
 listProbabilidades = []
 
 #Llenamos la lista con todos los simbolos y la lista con los simbolos unicos
-for symbol in itertools.chain.from_iterable(f):
+for symbol in fuente:
     if(symbol not in listSymbols and symbol != "\n"):
         listSymbols.append(symbol)
     if(symbol =="\n"):
         listAll.append(" ")
         listAll.append(" ")
-    listAll.append(symbol)
+    else:
+        listAll.append(symbol)
 
 #Contamos las apariciones de cada simbolo
-for symbol in listSymbols:
-    num = listAll.count(symbol)
-    listNumber.append(num)
+countAppereances(listAll,listNumber,listSymbols)
 
 #Contamos el numero total de apariciones
 count = 0
@@ -72,3 +78,63 @@ for i in range(0,4):
 
 #Cierra el archivo
 f.close()
+
+#Extra
+listAll2 = []
+listSymbols2 = []
+listNumber2 = []
+listProbabilidades2 = []
+
+#Llenamos la lista con todos los simbolos de dos en dos y la lista con los simbolos unicos de dos en dos
+f = open("datos_1.txt")
+fuente2 = f.read()
+
+for i in range(0,len(fuente2)+2,2):
+    symbol = listAll[i]+listAll[i+1]
+    listAll2.append(symbol)
+
+for i in range(0,len(listAll2)):
+    symbol = listAll2[i]
+    if(symbol not in listSymbols2):
+        listSymbols2.append(symbol)
+    
+#Contamos las apariciones de cada simbolo
+countAppereances(listAll2,listNumber2,listSymbols2)
+
+#Aparaciones totales
+count = count/2
+
+#Llenamos la lista con las probabilidades de cada simbolo
+for num in listNumber2:
+    listProbabilidades2.append(num/count)
+
+#Calculamos la entropia
+i=0
+sumatorio = 0
+for symbol in listSymbols2:
+    paso = listNumber2[i]*np.log(listNumber2[i])
+    sumatorio = sumatorio + paso
+    i+=1
+entropia = round(1/np.log(2)*(np.log(count)-1/count*sumatorio),4)
+
+#Ordenamos todas las listas
+for j in range (len(listProbabilidades2)):
+    swapped = False
+    i = 0
+    while i<len(listProbabilidades2)-1:
+        if listProbabilidades2[i] < listProbabilidades2[i+1]:
+            listProbabilidades2[i],listProbabilidades2[i+1] = listProbabilidades2[i+1],listProbabilidades2[i]
+            listSymbols2[i],listSymbols2[i+1] = listSymbols2[i+1],listSymbols2[i]
+            listNumber2[i],listNumber2[i+1] = listNumber2[i+1],listNumber2[i]
+            swapped = True
+        i=i+1
+    if swapped ==False:
+        break
+
+#Imprimimos las cuatro primeras  
+print("\nExtra: \nEntropia: " + str(entropia))
+for i in range(0,4):
+    roundValue = round(listProbabilidades2[i],5)
+    print(str(listSymbols2[i]) + " " + str(listNumber2[i]) + " " + str(roundValue))
+
+
